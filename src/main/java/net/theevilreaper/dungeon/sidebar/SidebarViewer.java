a7fed94f36f0e11a8c3b4680ba547cbcb3cbcac7
theEvilReaper <steffenwx@gmail.com>
2022-09-11 13:09:14 +0200

    Mark some variables as deprecated

--- src/main/java/net/theevilreaper/dungeon/sidebar/SidebarViewer.java
+++ src/main/java/net/theevilreaper/dungeon/sidebar/SidebarViewer.java
@@ -24,21 +24,15 @@ import java.util.UUID;
 public class SidebarViewer {
 
     private static final int GAME_MODE_SWITCHER_LEVEL = 2;
-
     private static final Component RANK_SPACE = Component.text("⍿ ", NamedTextColor.DARK_GRAY);
-
     private static final Logger LOGGER = LoggerFactory.getLogger(SidebarViewer.class);
-
     private static final TeamManager TEAM_MANAGER = MinecraftServer.getTeamManager();
-
     private static final Component ARROW_COMPONENT = Component.text(" ➟ ", NamedTextColor.GRAY);
-
     private static final Component TAB_HEADER = DungeonEditor.MINI_MESSAGE
             .deserialize("<color:#66ff66><bold>┏                                              <color:#ff6600><bold>┒\n")
             .append(Component.text("» ").style(Style.style(NamedTextColor.DARK_GRAY, TextDecoration.BOLD)))
             .append(DungeonEditor.MINI_MESSAGE.deserialize("Dungeon Editor"))
             .append(Component.text(" «").style(Style.style(NamedTextColor.DARK_GRAY, TextDecoration.BOLD))).append(Component.text("\n"));
-
     private static final Component TAB_FOOTER = Component.text("\n")
             .append(Component.text("Eat", NamedTextColor.YELLOW)
                     .append(ARROW_COMPONENT)
@@ -49,9 +43,10 @@ public class SidebarViewer {
                     .append(Component.text("repeat", NamedTextColor.RED))
             ).append(Component.text("\n")).append(DungeonEditor.MINI_MESSAGE.deserialize("<color:#ff6600><bold>┖                                              <color:#66ff66><bold>┛"));
 
+    @Deprecated(forRemoval = true)
     private static final UUID REAPER_UUID = UUID.fromString("6e2f3944-96d2-4e01-9056-dcab7f3937a4");
+    @Deprecated(forRemoval = true)
     private static final UUID SEELE_UUID = UUID.fromString("e1dff87a-1059-47f1-bc7d-533f072bf8c7");
-
     public SidebarViewer() {
         this.initTeams();
     }
@@ -96,5 +91,3 @@ public class SidebarViewer {
         player.removeTag(DungeonEditor.NAME_TAG);
     }
 }
-
-
