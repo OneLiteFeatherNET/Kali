a2b02fbe6f9a169b17418263a24dc90678fa7bba
theEvilReaper <steffenwx@gmail.com>
2023-01-25 13:49:27 +0100

    Add Tags class

---
+++ src/main/java/net/theevilreaper/dungeon/util/Tags.java
@@ -0,0 +1,10 @@
+package net.theevilreaper.dungeon.util;
+
+import net.minestom.server.tag.Tag;
+
+public final class Tags {
+
+    public static final Tag<Byte> ITEM_TAGS = Tag.Byte("floorItem");
+
+    private Tags() {}
+}
