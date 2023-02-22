c785ec7af93ae7bcba4eada6a5f776972584fa48
theEvilReaper <steffenwx@gmail.com>
2023-02-22 21:53:17 +0100

    Update tag usage

--- src/main/java/net/theevilreaper/dungeon/util/Tags.java
+++ src/main/java/net/theevilreaper/dungeon/util/Tags.java
@@ -10,6 +10,7 @@ public final class Tags {
     public static final Tag<Integer> NAME_TAG = Tag.Integer("displayTag");
     public static final Tag<UUID> UUID_TAG = Tag.UUID("id");
     public static final Tag<Byte> RESET_TAG = Tag.Byte("reset");
+    public static final Tag<String> DELETE_FLOOR = Tag.String("deleteFloor");
 
     private Tags() {}
 }
