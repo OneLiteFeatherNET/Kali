133c8b5b618c08589a43b5d62f76cbdf1a5727a6
theEvilReaper <steffenwx@gmail.com>
2023-03-24 00:03:57 +0100

    Add tag value from another class

--- src/main/java/net/theevilreaper/dungeon/util/Tags.java
+++ src/main/java/net/theevilreaper/dungeon/util/Tags.java
@@ -11,6 +11,7 @@ public final class Tags {
     public static final Tag<UUID> UUID_TAG = Tag.UUID("id");
     public static final Tag<Byte> RESET_TAG = Tag.Byte("reset");
     public static final Tag<String> DELETE_FLOOR = Tag.String("deleteFloor");
+    public static final Tag<String> FLOOR_TAG = Tag.String("name");
 
     private Tags() {}
 }
