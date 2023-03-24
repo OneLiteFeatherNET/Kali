e34edabd9893b2020ef5a412600e638cea3e0418
theEvilReaper <steffenwx@gmail.com>
2023-03-24 22:20:25 +0100

    Update floor events to the new structure

--- src/main/java/net/theevilreaper/dungeon/event/FloorRemoveEvent.java
+++ src/main/java/net/theevilreaper/dungeon/event/FloorRemoveEvent.java
@@ -3,6 +3,7 @@ package net.theevilreaper.dungeon.event;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.trait.PlayerEvent;
 import net.theevilreaper.dungeon.data.floor.Floor;
+import net.theevilreaper.dungeon.data.floor.FloorDTO;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -15,25 +16,25 @@ import org.jetbrains.annotations.NotNull;
 public final class FloorRemoveEvent implements PlayerEvent {
 
     private final Player player;
-    private final Floor floor;
+    private final Floor floorDTO;
 
     /**
      * Create a new instance of the {@link FloorCreateEvent}.
      * @param player the player who is involved into the event
-     * @param floor the floor who is involved into the event
+     * @param floorDTO the floor who is involved into the event
      */
-    public FloorRemoveEvent(@NotNull Player player, @NotNull Floor floor) {
+    public FloorRemoveEvent(@NotNull Player player, @NotNull Floor floorDTO) {
         this.player = player;
-        this.floor = floor;
+        this.floorDTO = floorDTO;
     }
 
     /**
-     * Returns the involved {@link Floor}.
+     * Returns the involved {@link FloorDTO}.
      * @return the given floor
      */
     @NotNull
     public Floor getFloor() {
-        return this.floor;
+        return this.floorDTO;
     }
 
     /**
