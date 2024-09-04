2c410131432c062fdd77dfe917486b2301ccb59f
theEvilReaper <steffenwx@gmail.com>
2024-09-04 10:28:28 +0200

    Java version bump and some dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,14 @@ rootProject.name = "Kali"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("minestom", "net.onelitefeather.microtus", "Minestom").version("1.1.1")
-            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("1.1.1")
+            library("minestom", "net.onelitefeather.microtus", "Minestom").version("1.4.2")
+            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("1.4.2")
             library("adventure.minimessage", "net.kyori", "adventure-text-minimessage").version("4.14.0")
             library("junit.api", "org.junit.jupiter", "junit-jupiter-api").version("5.10.0")
             library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.0")
             library("mockito.core", "org.mockito", "mockito-core").version("5.6.0")
             library("mockito.junit", "org.mockito", "mockito-junit-jupiter").version("5.6.0")
-            library("aves", "de.icevizion.lib", "Aves").version("1.2.0+e27b7048")
+            library("aves", "de.icevizion.lib", "Aves").version("1.5.2")
             library("canis", "com.github.theEvilReaper", "Canis").version("master-SNAPSHOT")
             library("mini", "net.kyori", "adventure-text-minimessage").version("4.14.0")
             library("morphia", "dev.morphia.morphia", "morphia-core").version("2.2.8")
